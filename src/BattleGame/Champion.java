52b4c1331d190e6374bf86e9d293fae41f8df216
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-05-10 20:33:59 -0300

    criação da classe champion

---
+++ src/BattleGame/Champion.java
@@ -0,0 +1,9 @@
+package BattleGame;
+
+public class Champion {
+	
+	public String name;
+	public int life;
+	public int attack;
+	public int armor;
+}
---
+++ src/BattleGame/Program.java
@@ -0,0 +1,14 @@
+package BattleGame;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int x;
+		x = 10;
+		
+		System.out.print(x);
+
+	}
+
+}
